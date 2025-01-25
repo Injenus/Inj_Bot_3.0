3f132e134ee2255e9d2c7cd8748500ba3fab4415
injenus <injenus777@gmail.com>
2025-01-26 00:52:56 +0300

    edit servo settings

--- arm_n_cam_mount_api_8_bytes/main/main.ino
+++ arm_n_cam_mount_api_8_bytes/main/main.ino
@@ -5,7 +5,7 @@
    5 - основание камеры
    6 - высота камеры
 */
-#include "ServoDriverSmooth.h"
+#include <ServoDriverSmooth.h>
 #include <EEPROM.h>
 
 const byte servoNum = 7;
@@ -14,10 +14,14 @@ const uint16_t addr = 42;
 ServoDriverSmooth servo[servoNum];
 
 struct ServoSettings {
-  int min_mcs[servoNum] = {84, 116, 116, 116, 300, 135, 150};
-  int max_mcs[servoNum] = {582, 542, 542, 542, 492, 525, 467};
-  int min_angles[servoNum] = {0, 0, 0, 0, 0, 0, 0};
-  int max_angles[servoNum] = {270, 270, 270, 270, 100, 180, 100};
+  int minMcs[servoNum] = {84, 116, 116, 116, 300, 135, 150};
+  int maxMcs[servoNum] = {582, 542, 542, 542, 492, 525, 467};
+  int minAngles[servoNum] = {0, 0, 0, 0, 0, 0, 0};
+  int maxAngles[servoNum] = {270, 270, 270, 270, 100, 180, 100};
+  int armAcc = 80;
+  int armSpeed = 1080;
+  int camAcc = 480;
+  int camSpeed = 1080;
 };
 ServoSettings servoSettings;
 
@@ -41,10 +45,10 @@ uint8_t fletcher8(uint8_t *data, size_t len) {
 }
 
 int anglesToMcs(int angles, byte idx) {
-  int from_a_min = servoSettings.min_angles[idx];
-  int from_a_max = servoSettings.max_angles[idx];
-  int to_mcs_min = servoSettings.min_mcs[idx];
-  int to_mcs_max = servoSettings.max_mcs[idx];
+  int from_a_min = servoSettings.minAngles[idx];
+  int from_a_max = servoSettings.maxAngles[idx];
+  int to_mcs_min = servoSettings.minMcs[idx];
+  int to_mcs_max = servoSettings.maxMcs[idx];
   return map(angles, from_a_min, from_a_max, to_mcs_min, to_mcs_max);
 }
 
@@ -53,47 +57,47 @@ void setup() {
   EEPROM.get(addr, servoPosAng);
   Serial.println(servoPosAng.pos[0]);
 
-  servo[0].attach(0, servoSettings.min_mcs[0], servoSettings.max_mcs[0]);
+  servo[0].attach(0, servoSettings.minMcs[0], servoSettings.maxMcs[0]);
   servo[0].writeMicroseconds(anglesToMcs(servoPosAng.pos[0], 0));
   servo[0].setAutoDetach(false);
-  servo[0].setSpeed(240);
-  servo[0].setAccel(40);
+  servo[0].setSpeed(servoSettings.armSpeed);
+  servo[0].setAccel(servoSettings.armAcc);
 
-  servo[1].attach(1, servoSettings.min_mcs[1], servoSettings.max_mcs[1]);
+  servo[1].attach(1, servoSettings.minMcs[1], servoSettings.maxMcs[1]);
   servo[1].writeMicroseconds(anglesToMcs(servoPosAng.pos[1], 1));
   servo[1].setAutoDetach(false);
-  servo[1].setSpeed(240);
-  servo[1].setAccel(40);
+  servo[1].setSpeed(servoSettings.armSpeed);
+  servo[1].setAccel(servoSettings.armAcc);
 
-  servo[2].attach(2, servoSettings.min_mcs[2], servoSettings.max_mcs[2]);
+  servo[2].attach(2, servoSettings.minMcs[2], servoSettings.maxMcs[2]);
   servo[2].writeMicroseconds(anglesToMcs(servoPosAng.pos[2], 2));
   servo[2].setAutoDetach(false);
-  servo[2].setSpeed(240);
-  servo[2].setAccel(40);
+  servo[2].setSpeed(servoSettings.armSpeed);
+  servo[2].setAccel(servoSettings.armAcc);
 
-  servo[3].attach(3, servoSettings.min_mcs[3], servoSettings.max_mcs[3]);
+  servo[3].attach(3, servoSettings.minMcs[3], servoSettings.maxMcs[3]);
   servo[3].writeMicroseconds(anglesToMcs(servoPosAng.pos[3], 3));
   servo[3].setAutoDetach(false);
-  servo[3].setSpeed(240);
-  servo[3].setAccel(40);
+  servo[3].setSpeed(servoSettings.armSpeed);
+  servo[3].setAccel(servoSettings.armAcc);
 
-  servo[4].attach(4, servoSettings.min_mcs[4], servoSettings.max_mcs[4]);
+  servo[4].attach(4, servoSettings.minMcs[4], servoSettings.maxMcs[4]);
   servo[4].writeMicroseconds(anglesToMcs(servoPosAng.pos[4], 4));
   servo[4].setAutoDetach(false);
-  servo[4].setSpeed(240);
-  servo[4].setAccel(40);
+  servo[4].setSpeed(servoSettings.armSpeed);
+  servo[4].setAccel(servoSettings.armAcc);
 
-  servo[5].attach(5, servoSettings.min_mcs[5], servoSettings.max_mcs[5]);
+  servo[5].attach(5, servoSettings.minMcs[5], servoSettings.maxMcs[5]);
   servo[5].writeMicroseconds(anglesToMcs(servoPosAng.pos[5], 5));
   servo[5].setAutoDetach(false);
-  servo[5].setSpeed(360);
-  servo[5].setAccel(360);
+  servo[5].setSpeed(servoSettings.camSpeed);
+  servo[5].setAccel(servoSettings.camAcc);
 
-  servo[6].attach(6, servoSettings.min_mcs[6], servoSettings.max_mcs[6]);
+  servo[6].attach(6, servoSettings.minMcs[6], servoSettings.maxMcs[6]);
   servo[6].writeMicroseconds(anglesToMcs(servoPosAng.pos[6], 6));
   servo[6].setAutoDetach(false);
-  servo[6].setSpeed(360);
-  servo[6].setAccel(360);
+  servo[6].setSpeed(servoSettings.camSpeed);
+  servo[6].setAccel(servoSettings.camAcc);
 
 }
 
@@ -112,13 +116,35 @@ void loop() {
   servo[5].tick();
   servo[6].tick();
 
+//  if (servo[0].tick()){
+//    servo[0].writeMicroseconds(servo[0].getTarget());
+//  }
+//  if (servo[1].tick()){
+//    servo[1].writeMicroseconds(servo[1].getTarget());
+//  }
+//  if (servo[2].tick()){
+//    servo[2].writeMicroseconds(servo[2].getTarget());
+//  }
+//  if (servo[3].tick()){
+//    servo[3].writeMicroseconds(servo[3].getTarget());
+//  }
+//  if (servo[4].tick()){
+//    servo[4].writeMicroseconds(servo[4].getTarget());
+//  }
+//  if (servo[5].tick()){
+//    servo[5].writeMicroseconds(servo[5].getTarget());
+//  }
+//  if (servo[6].tick()){
+//    servo[6].writeMicroseconds(servo[6].getTarget());
+//  }
+
   while (Serial.available() > 0) {
     uint8_t byteReceived = Serial.read();
 
     // Если это первый байт и он равен 'A', начинаем запись в буфер
     if (index == 0 && byteReceived == 'A') {
       rBuffer[index++] = byteReceived;
-      Serial.println("Got A");
+//      Serial.println("Got A");
     }
     // Если уже начали запись, продолжаем добавлять байты в буфер
     else if (index > 0 && index < 9) {
@@ -126,7 +152,7 @@ void loop() {
     
       // Если буфер заполнен, проверяем данные
       if (index == 9) {
-        Serial.println("Got all");
+//        Serial.println("Got all");
         // Вычисляем контрольную сумму для первых 8 байт
         uint8_t receivedChecksum = rBuffer[8];
         uint8_t calculatedChecksum = fletcher8(rBuffer, 8);
@@ -138,20 +164,21 @@ void loop() {
             servoPosAng.pos[i] = rBuffer[1 + i];
           }
     
-          Serial.println("Right!");
+//          Serial.println("Right!");
           for (size_t i = 0; i < 7; i++) {
-            Serial.print("mcS ");
-            Serial.print(i);
-            Serial.print(": ");
-            Serial.println(anglesToMcs(servoPosAng.pos[i], i));
-            servo[i].writeMicroseconds(anglesToMcs(servoPosAng.pos[i], i));
+//            Serial.print("mcS ");
+//            Serial.print(i);
+//            Serial.print(": ");
+//            Serial.println(anglesToMcs(servoPosAng.pos[i], i));
+            //servo[i].writeMicroseconds(anglesToMcs(servoPosAng.pos[i], i));
+            servo[i].setTarget(anglesToMcs(servoPosAng.pos[i], i));
           }
           EEPROM.put(addr, servoPosAng);
         } else {
-          Serial.print(receivedChecksum);
-          Serial.print("  ");
-          Serial.print(calculatedChecksum);
-          Serial.println(" Error");
+//          Serial.print(receivedChecksum);
+//          Serial.print("  ");
+//          Serial.print(calculatedChecksum);
+//          Serial.println(" Error");
         }
         // Сбрасываем индекс буфера для следующего сообщения
         index = 0;
@@ -162,6 +189,5 @@ void loop() {
     }
 
   }
-
 
 }
