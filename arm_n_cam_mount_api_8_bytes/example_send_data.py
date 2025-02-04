b5d979935ab78bd733c5e8ef976a97f76c97e079
injenus <injenus777@gmail.com>
2025-02-04 16:00:26 +0300

    update send example, edit angles range

--- arm_n_cam_mount_api_8_bytes/example_send_data.py
+++ arm_n_cam_mount_api_8_bytes/example_send_data.py
@@ -1,4 +1,6 @@
 import serial
+import json
+
 import arm_n_cam_mount_api_8_bytes.send_data as send_data
 
 port = "COM5"
@@ -11,9 +13,18 @@ ser = serial.Serial(port, baud_rate, timeout=1)
 #     print(f'Failed to open {port} port!')
 #     exit(-42)
 
-while True:
-    angles = [130, 140, 120, 145, 125, 130, 150] # Дефолтные значения
+with open('config_arm.json', 'r') as file:
+        config = json.load(file)
+        arm_offet = config['offset']
 
+with open('config_cam.json', 'r') as file:
+        config = json.load(file)
+        cam_offet = config['offset']
+
+offset = arm_offet + cam_offet
+while True:
+    user_angles = [45, 10, 0, -100, 1, 0, 0] # Дефолтные
+    
     input_ang = input() # Ожидается не более 6 чисел, дфеолтные соответствующие порядку числа будут заменены на введённые
     if input_ang == 'f':
         break
@@ -23,10 +34,15 @@ while True:
             print("More than 6")
             break
         try:
-            angles[i] = int(el)
+            ang = int(el)
+            if -128 <= ang <= 127:
+                user_angles[i] = ang
+            else:
+                 print(f"Угол {i} не был заменён: выход за пределы диапазона")
         except:
             print("Exc")
 
+    angles = [ang + offset[i] for i, ang in enumerate(user_angles)]
     send_data.send_to_serial(ser, angles)
 
 ser.close()
