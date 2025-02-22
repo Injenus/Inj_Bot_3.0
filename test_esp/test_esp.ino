f05c85e35176d1b7bb60fab9e48bdb67c0f1c508
injenus <injenus777@gmail.com>
2025-02-22 05:02:05 +0300

    add test script for esp wheel control

---
+++ test_esp/test_esp.ino
@@ -0,0 +1,92 @@
+// --- Назначение пинов энкодеров ---
+#define ENC1_A 32
+#define ENC1_B 33
+#define ENC2_A 25
+#define ENC2_B 26
+#define ENC3_A 27
+#define ENC3_B 14
+#define ENC4_A 15  // заменили с 12 на 15 для безопасности
+#define ENC4_B 2   // безопасный пин для второго канала
+
+// --- Назначение ШИМ-пинов ---
+#define PWM1 4
+#define PWM2 16
+#define PWM3 17
+#define PWM4 18
+#define PWM5 19
+#define PWM6 21
+#define PWM7 22
+#define PWM8 23
+#define PWM_DEF 5
+
+#define PWM_FREQ 16000
+#define PWM_RESOL 12
+
+volatile long encoderPositions[4] = {0, 0, 0, 0};
+
+// --- Обработчики прерываний для энкодеров ---
+void IRAM_ATTR handleEncoder1A() { encoderPositions[0] += (digitalRead(ENC1_A) == digitalRead(ENC1_B)) ? 1 : -1; }
+void IRAM_ATTR handleEncoder1B() { encoderPositions[0] += (digitalRead(ENC1_A) != digitalRead(ENC1_B)) ? 1 : -1; }
+
+void IRAM_ATTR handleEncoder2A() { encoderPositions[1] += (digitalRead(ENC2_A) == digitalRead(ENC2_B)) ? 1 : -1; }
+void IRAM_ATTR handleEncoder2B() { encoderPositions[1] += (digitalRead(ENC2_A) != digitalRead(ENC2_B)) ? 1 : -1; }
+
+void IRAM_ATTR handleEncoder3A() { encoderPositions[2] += (digitalRead(ENC3_A) == digitalRead(ENC3_B)) ? 1 : -1; }
+void IRAM_ATTR handleEncoder3B() { encoderPositions[2] += (digitalRead(ENC3_A) != digitalRead(ENC3_B)) ? 1 : -1; }
+
+void IRAM_ATTR handleEncoder4A() { encoderPositions[3] += (digitalRead(ENC4_A) == digitalRead(ENC4_B)) ? 1 : -1; }
+void IRAM_ATTR handleEncoder4B() { encoderPositions[3] += (digitalRead(ENC4_A) != digitalRead(ENC4_B)) ? 1 : -1; }
+
+void setupEncoder(uint8_t pinA, uint8_t pinB, void (*isrA)(), void (*isrB)()) {
+  pinMode(pinA, INPUT_PULLUP);
+  pinMode(pinB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinA), isrA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pinB), isrB, CHANGE);
+}
+
+void setupPWM(uint8_t pin) {
+    ledcAttach(pin, PWM_FREQ, PWM_RESOL);
+    ledcWrite(pin, 3000);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(115200);
+
+  // --- Настройка энкодеров ---
+  setupEncoder(ENC1_A, ENC1_B, handleEncoder1A, handleEncoder1B);
+  setupEncoder(ENC2_A, ENC2_B, handleEncoder2A, handleEncoder2B);
+  setupEncoder(ENC3_A, ENC3_B, handleEncoder3A, handleEncoder3B);
+  setupEncoder(ENC4_A, ENC4_B, handleEncoder4A, handleEncoder4B);
+
+  // --- Настройка ШИМ (каналы с 0 по 7) ---
+  setupPWM(PWM_DEF);
+  setupPWM(PWM1);
+  setupPWM(PWM2);
+  setupPWM(PWM3);
+  setupPWM(PWM4);
+  setupPWM(PWM5);
+  setupPWM(PWM6);
+  setupPWM(PWM7);
+  setupPWM(PWM8);
+
+  Serial.println("Система с 4 энкодерами и 8 ШИМ-пинами запущена");
+}
+
+void loop() {
+  static unsigned long lastPrintTime = 0;
+  unsigned long currentTime = millis();
+
+  if (currentTime - lastPrintTime >= 10) {  // 100 Гц обновление
+    lastPrintTime = currentTime;
+
+    noInterrupts();
+    long positions[4] = {encoderPositions[0], encoderPositions[1], encoderPositions[2], encoderPositions[3]};
+    interrupts();
+
+    char buffer[100];
+    snprintf(buffer, sizeof(buffer), "E1:%ld E2:%ld E3:%ld E4:%ld", positions[0], positions[1], positions[2], positions[3]);
+    Serial.println(buffer);
+    Serial2.println(buffer);
+  }
+}
