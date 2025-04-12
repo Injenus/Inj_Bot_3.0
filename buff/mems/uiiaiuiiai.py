44074b67058e9387265319900e5907b5dc8201a1
injenus <injenus777@gmail.com>
2025-04-13 01:40:05 +0300

    add mem uiiai

---
+++ buff/mems/uiiaiuiiai.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import json
+from std_msgs.msg import UInt8
+from geometry_msgs.msg import Twist
+import copy
+import numpy as np
+import time
+import bisect
+
+modules_data_path = os.path.join(os.path.expanduser('~'), 'Inj_Bot_3.0', 'modules')
+if modules_data_path not in sys.path:
+    sys.path.append(modules_data_path)
+
+from play_audio import play_audio
+
+"""
+ 0 включаем песню
+ 1 - вращаемся по час
+ 2 - пауза
+ 3 - вращемся прот час
+"""
+
+
+class Uiiaiuiiai(Node):
+    def __init__(self):
+        super().__init__('uiiaiuiiai')
+
+        self.publ_twist = self.create_publisher(Twist, '/cmd_vel', 3)
+
+        self.timer = self.create_timer(0.005, self.loop)
+
+
+        self.pause = self.create_timer(2.0, self.wait)
+        self.pause.reset() # отмена пертиодического вызова
+
+        self.resume = self.create_timer(3.0, self.change_direction)
+        self.resume.reset()
+
+        self.node_stop = self.create_timer(5.1, self.destroy_node)
+        self.node_stop.reset()
+
+
+    def wait(self):
+        self.main_state = 2
+        self.pause.cancel()
+
+    def change_direction(self):
+        self.main_state = 3
+        self.resume.cancel() 
+
+
+    def loop(self):
+        msg = Twist()
+
+        if self.main_state == 0:
+            play_audio('uiiaiuiiai.wav')
+            self.main_state = 1
+
+        elif self.main_state == 1:
+            msg.angular.z = 3.14
+
+        elif self.main_state == 2:
+            msg.angular.z = 0.0
+
+        elif self.main_state == 3:
+            msg.angular.z = -6.28
+
+        self.publ_twist.publish(msg)
+
+    
+    def destroy_node(self):
+        self.main_state = 4
+        self.node_stop.cancel()
+        super().destroy_node()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    uiiaiuiiai = Uiiaiuiiai()
+    rclpy.spin(uiiaiuiiai)
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ ros2_inj_bot_3.0/src/mems_uiiaiuiiai.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCENARIO=${1:-"default"}  # Получаем первый аргумент или используем "default"
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
+
+amixer set Master 65535
+
+if [ "$SCENARIO" = "omni" ]; then
+    bash -c "source ../../venv/bin/activate && source ../install/setup.bash && ros2 run fuck_wheel twist_to_rpm_omni" &
+else
+    bash -c "source ../../venv/bin/activate && source ../install/setup.bash && ros2 run fuck_wheel twist_to_rpm" &
+fi
+
+bash -c "source ../../venv/bin/activate && source ../install/setup.bash && ros2 run fuck_wheel data_exchange" &
+bash -c "source ../../venv/bin/activate && source ../install/setup.bash && ros2 run mems uiiaiuiiai" &
+
+amixer set Master 16384
+
